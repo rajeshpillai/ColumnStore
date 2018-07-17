7faa1f830f30223e916f7a20e462962aa40c5874
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-07-17 09:56:36 +0530

    updated

--- ColumnStore/Program.cs
+++ ColumnStore/Program.cs
@@ -2,48 +2,162 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Globalization;
+using System.Text;
 
 namespace ColumnStore
 {
+    /*
+	    An Experimental Columnar DB using c#
+	    Author: Rajesh Pillai
+    */
+
     class Database
     {
-        public List<Table> Tables { get; set; }
+        public Dictionary<string, Table> Tables { get; set; }
         public Database()
         {
-            this.Tables = new List<Table>();
+            this.Tables = new Dictionary<string, Table>();
+        }
+
+        public object Query(dynamic param)
+        {
+            var result = new Dictionary<int, string>();
+
+            Console.WriteLine(param);
+            var table = this.Tables[param.Table];
+            var column = table.Columns[param.Field];
+            var filterValue = param.Value;
+            var select = param.Select;
+
+            var selectColumn = table.Columns[select];
+
+            Console.WriteLine(table.Columns.Keys);
+
+            StringBuilder record = new StringBuilder();
+
+
+            for (int row = 0; row < selectColumn.Values.Count; row++)
+            {
+                foreach (var col in table.Columns)
+                {
+                    //Console.WriteLine(col.Value);
+                    record.Append(col.Value.Values[row] + ",");
+                }
+                record.AppendLine();
+            }
+            Console.WriteLine(record.ToString());
+
+            /*
+            foreach(var c in table.Columns) {
+                Console.WriteLine(c.Key);
+                foreach(var val in c.Value.Values) {
+                    if (val.Value.ToLower() == filterValue.ToLower()) {
+                        result.Add(val.Key, selectColumn.Values[val.Key]);
+                    }
+                }
+            }
+            */
+
+            return result;
         }
     }
 
-    class Table {
+    class Table
+    {
 
         public Table(string name)
         {
             this.Name = name;
-            this.Columns = new List<Column>();
+            this.Columns = new Dictionary<string, Column>();
         }
-        public List<Column> Columns { get; set; }
+        public Dictionary<string, Column> Columns { get; set; }
+
+
         public string Name { get; set; }
+
+        /*
+        public string this[int i]
+        {
+            get {
+                return names[i];
+            }
+            set {
+                names[i] = value;
+            }
+        }
+        */
+
+
+
+        public DataTable ToDataTable()
+        {
+            DataTable result = new DataTable();
+
+            if (Columns.Count == 0)
+                return result;
+
+            foreach (KeyValuePair<string, Column> col in this.Columns)
+            {
+                result.Columns.Add(col.Key);
+            }
+
+            bool processed = false;
+
+            int i = 0;
+            int cols = this.Columns.Count
+                ;
+            foreach (KeyValuePair<string, Column> col in this.Columns)
+            {
+                foreach (var value in col.Value.Values)
+                {
+                    DataRow row = null;
+                    if (!processed)
+                    {
+                        row = result.NewRow();
+                    }
+                    else
+                    {
+                        row = result.Rows[i++];
+                    }
+                    row[col.Key] = value.Value;
+
+
+                    if (!processed) result.Rows.Add(row);
+                }
+                processed = true;
+                i = 0;
+            }
+            return result;
+        }
     }
 
-    class Column {
+    class Column
+    {
         public string Name { get; set; }
-        public Dictionary<int,string> Values { get; set; }
+        public Dictionary<int, string> Values { get; set; }
 
         public Column()
         {
             this.Values = new Dictionary<int, string>();
         }
 
-        public Column(string Name) : this() 
+        public Column(string Name) : this()
         {
             this.Name = Name;
         }
 
         public override string ToString()
         {
-            return string.Format("{0}", Values.ToString());
+            StringBuilder sb = new StringBuilder();
+            foreach (var col in this.Values)
+            {
+                sb.Append(col.Value);
+            }
+            return string.Format("{0}", sb.ToString());
         }
     }
+
+    
 
 
     class Program
@@ -51,31 +165,60 @@ namespace ColumnStore
         static Database db = new Database();
         static void Main(string[] args)
         {
-            var employee = BuildEmployeeTable();
-            Console.WriteLine(employee.Columns.Count);
-            Console.ReadKey();
+            db = new Database();
+            var employees = BuildEmployeeTable();
+            var skills = BuildSkillsTable();
+
+
+            var query = new
+            {
+                Table = "employees",
+                Field = "city",
+                Operator = "=",
+                Value = "chennai",
+                Select = "ename"
+            };
+
+            var mumbai = db.Query(query);
+            Console.WriteLine(mumbai);
+            Console.ReadLine();
         }
 
 
         static Table BuildEmployeeTable()
         {
             var table = new Table("employees");
-            db.Tables.Add(table);
-            table.Columns.Add(new Column("name"));
-            table.Columns.Add(new Column("city"));
+            db.Tables.Add("employees", table);
+            table.Columns.Add("ename", new Column("ename"));
+            table.Columns.Add("city", new Column("city"));
+            table.Columns.Add("empid", new Column("empid"));
 
-            table.Columns[0].Values.Add(1, "jai");
-            table.Columns[0].Values.Add(2, "urvashi");
-            table.Columns[0].Values.Add(3, "rajesh");
-            table.Columns[0].Values.Add(4, "smeeta");
 
-            table.Columns[1].Values.Add(1, "delhi");
-            table.Columns[1].Values.Add(2, "mumbai");
-            table.Columns[1].Values.Add(3, "kerala");
-            table.Columns[1].Values.Add(4, "mumbai");
+            for (int i = 0; i < 10; i++)
+            {
+                table.Columns["ename"].Values.Add(i, "name " + i.ToString());
+                table.Columns["city"].Values.Add(i, (i % 2 == 0 ? "mumbai" : "chennai"));
+                table.Columns["empid"].Values.Add(i, i.ToString());
 
+            }
             return table;
         }
 
+        static Table BuildSkillsTable()
+        {
+            var table = new Table("skills");
+            db.Tables.Add("skills", table);
+            table.Columns.Add("empid", new Column("empid"));
+            table.Columns.Add("skill", new Column("skill"));
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                table.Columns["empid"].Values.Add(i, i.ToString());
+                table.Columns["skill"].Values.Add(i, (i % 2 == 0 ? "nodejs" : ".net"));
+
+            }
+            return table;
+        }
     }
 }
